f6a57b1f1438ebf70f112dc98d5c11f9e7af01cc
mahesh kose <ms2kose@gmail.com>
2026-03-31 14:07:03 +0530

    database url fix

--- lib/dbconnect.ts
+++ lib/dbconnect.ts
@@ -3,7 +3,14 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL ,{dbName: "personal" });
+      const dbUrl = process.env.DATABASE_URL;
+
+    if (!dbUrl) {
+      console.error("DATABASE_URL is not defined");
+      throw new Error("DATABASE_URL is not defined");
+    }
+
+    await mongoose.connect(dbUrl, { dbName: "personal" });
     console.log("Database connected succefully");
   } catch(error:any){
     console.log("Database connection Error",error.message);
