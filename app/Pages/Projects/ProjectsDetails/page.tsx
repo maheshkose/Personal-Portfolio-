f44f20ba94c5adc76bf8d90447b8a37b4a86da9b
mahesh kose <ms2kose@gmail.com>
2026-04-02 14:22:57 +0530

    params id

--- app/Pages/Projects/ProjectsDetails/page.tsx
+++ app/Pages/Projects/ProjectsDetails/page.tsx
@@ -4,9 +4,9 @@ import projects from '@/lib/Projects';
 import { useSearchParams } from 'next/navigation';
 import { CardImage } from '@/app/components/projectcard';
 
-const page = () => {
+const page = ({params}:{params:{id:string}}) => {
   const queryparam = useSearchParams();
-  const id:string = queryparam.get("id")!;
+  const id:string = params.id;
   console.log(id);
   return (
     <div className='py-20 dark:bg-gray-900'><CardImage project={projects[Number(id)]}/></div>
